9bc6a396d6e2b4ee2a4cc78468975033a31c885f
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2023-05-07 15:43:20 +0900

    #4.1 - #4.3
    event.preventDefault()

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
+const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const loginButton = document.querySelector('#login-form button');
+const link = document.querySelector('a');
 
-function onLoginBtnClick(){
-    console.log(loginInput.value);
-    console.log('click!!');
+function onLoginSubmit(event){
+    // const username=loginInput.value;
+    event.preventDefault(); // form의 기본동작(submit 하면 브라우저 새로고침 되는 것)을 실행하지 못하게 하기
+    // console.log(username);
+    // if (username === '') {
+    //     alert('Please Write Your Name');
+    // } else if (username.length>15) {
+    //     alert('Your name is Too Long');
+    // }
 }
 
-loginButton.addEventListener('click', onLoginBtnClick);
+function handleLinkClick(event) {
+    event.preventDefault();
+    alert('clicked!');
+}
+
+
+loginForm.addEventListener('submit', onLoginSubmit);
+link.addEventListener('click', handleLinkClick);
