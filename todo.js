e3728fba56117fa3adc73d13fec166181dfabb1f
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2023-05-12 22:24:24 +0900

    #7.4 - #7.9  TodoList (2)
    JSON.stringify: string으로 만듦
    JSON.parse: JS가 사용가능한 object로 만듦
    .filter: return true 아닌 item을 제외하고 새로운 array 만듦

--- todo.js
+++ todo.js
@@ -1,21 +1,26 @@
 const todoForm = document.querySelector('#todo-form');
 const todoList = document.querySelector('#todo-list');
 const todoInput = todoForm.querySelector('input');
-const todos = [];
+let todos = [];     //업데이트 가능
+const TODOS_KEY = "todos";
+
 
 function saveTodos() {
-    localStorage.setItem('todos', todos);
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));     //string 만듦
 }
 
 function deleteTodo(event) {
     const li = event.target.parentElement; //삭제하고 싶은 element(각 삭제버튼이 포함된 li)
     li.remove();
+    todos = todos.filter((todos)=>todos.id !== parseInt(li.id));    //li.id는 string이므로 num으로 만들어줌
+    saveTodos();
 }
 
 function paintTodo(newTodo){
     const li = document.createElement('li');
+    li.id = newTodo.id;
     const span = document.createElement('span');
-    span.innerText = newTodo;  
+    span.innerText = newTodo.text ;  
     const button = document.createElement('button');    //삭제버튼
     button.innerText = '❌';
     button.addEventListener('click', deleteTodo);
@@ -28,9 +33,21 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value="";     //작성 후 엔터 누르면 input안이 비어지도록
-    todos.push(newTodo);    //입력받은 값을 array에 넣음
-    paintTodo(newTodo);     
+    const newTodoObj = {
+        id:Date.now(),
+        text:newTodo,
+    }
+    todos.push(newTodoObj);    //입력받은 값을 array에 넣음
+    paintTodo(newTodoObj);     
     saveTodos();            //입력받은 값을 브라우저에 저장
 }
 
 todoForm.addEventListener('submit', handleToDoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos !== null) {      //값이 존재한다면
+    const parsedTodos = JSON.parse(savedTodos);     //JS가 사용가능한 object로 만듦
+    todos = parsedTodos;        //localStorage에서 발견되는 이전의 todo들 포함하도록 
+    parsedTodos.forEach(paintTodo);
+}
